30e1180c979c0bd9dc961075feba4d31c8c8261d
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-08-31 15:10:32 +0200

    Initial mui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { theme } from './constants';
+import AppRoutes from './routes';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <AppRoutes />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
