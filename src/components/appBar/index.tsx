87cf4a207dd6ce101a4c676bc1feb499df16d8c7
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-08-31 22:22:24 +0200

    Create appBar component

---
+++ src/components/appBar/index.tsx
@@ -0,0 +1,13 @@
+import { Toolbar } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import React from 'react';
+
+const MenuAppBar = () => {
+  return (
+    <AppBar position='static'>
+      <Toolbar />
+    </AppBar>
+  );
+};
+
+export default MenuAppBar;
